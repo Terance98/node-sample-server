1a59281dc5930300e1ec1b3dfa70b8c523e04fdf
Thomas Terance <thomasterance@Minion.local>
2023-11-22 14:56:24 +0530

    minor updates

--- index.js
+++ index.js
@@ -4,6 +4,4 @@ const app = express();
 
 app.get("/ping", (req, res) => res.json({ data: "pong" }));
 
-app.get("/peechu", (req, res) => res.json({ data: "I love you peeechukutty" }));
-
-app.listen(80, () => console.log("App running on port 80"));
+app.listen(4000, () => console.log("App running on port 4000"));
