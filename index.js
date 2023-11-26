33dad2c3e7b547de3d80bda84e1351f3b28fb3be
terance98 <thomasterance98@gmail.com>
2023-11-26 20:11:13 +0530

    test update

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ app.get("/ping", (req, res) => res.json({ data: "pong" }));
 
 app.get("/", (req, res) => res.json({ data: "Hello from a sample node server!" }));
 
+app.get('/test', (req, res) => res.json({data: "Test Successful!"}));
+
 app.listen(4000, () => console.log("App running on port 4000"));
